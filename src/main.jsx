e38a2f3ddfb72f322bd0fd19aaec44f8168b431d
mokuzaru <kenny.otoya@pucp.pe>
2024-04-05 18:46:00 -0500

    use rtk query

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { PokemonApp } from './PokemonApp.jsx'
 import { store } from './store/Store.js'
+import { TodoApp } from './TodoApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <PokemonApp/>
+      <TodoApp/>
     </Provider>
     
   </React.StrictMode>,
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { counterSlice } from './slices/counter'
 import { pokemonSlice } from './slices/pokemon/PokemonSlice'
+import { todosApi } from './api'
 
 export const store =  configureStore({
   reducer: {
     counter: counterSlice.reducer,
     pokemons: pokemonSlice.reducer,
+
+    [todosApi.reducerPath]: todosApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat( todosApi.middleware)
 })
